cf8b93fe5c1faf6cd32a8bca8b846d9f6ecd6881
Bình Nguyễn Thanh <binh.nt@teko.vn>
2021-08-29 10:57:43 +0700

    fix test api rate limit

--- client-api/main.go
+++ client-api/main.go
@@ -21,8 +21,8 @@ func TestLimitRate() {
 	var wg sync.WaitGroup
 
 	numWorker := 5
-	limitRequest := 10
-	queue := make(chan int, limitRequest)
+
+	queue := make(chan int)
 	fmt.Println("Start total of ", numWorker)
 	for i := 0; i < numWorker; i++ {
 		wg.Add(1)
@@ -30,12 +30,12 @@ func TestLimitRate() {
 			fmt.Println("Start worker number ", id)
 			wg.Done()
 			for msg := range queue {
-				fmt.Printf("Worker %d received request %d\n", id, msg)
+				fmt.Printf("Worker %d send request %d\n", id, msg)
 
-				time.Sleep(1 * time.Second)
-				// accountId := fmt.Sprintf("Account.%d", msg)
-				// traceId := fmt.Sprintf("Trace.%d", time.Now())
-				// createAccount(traceId, accountId)
+				// time.Sleep(1 * time.Second)
+				accountId := fmt.Sprintf("Account.%d", msg)
+				traceId := fmt.Sprintf("Trace.%d", time.Now())
+				createAccount(msg, traceId, accountId)
 			}
 
 		}(i, queue)
@@ -44,23 +44,24 @@ func TestLimitRate() {
 
 	//Send message job
 	maxRequest := 20
-	fmt.Printf("\nStart send %d requests\n", maxRequest)
-	for i := 0; i < maxRequest; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		fmt.Printf("\nStart send %d requests\n", maxRequest)
+		for i := 0; i < maxRequest; i++ {
+			// time.Sleep(500 * time.Millisecond)
 			queue <- i
+		}
 
-		}(i)
-	}
+		time.Sleep(20 * time.Second)
+	}()
 
+	// time.Sleep(20 * time.Second)
 	wg.Wait()
-
-	time.Sleep(5 * time.Second)
-
 	fmt.Println("Main: Completed")
 }
-func createAccount(trace_no string, account_id string) {
+func createAccount(requestId int, trace_no string, account_id string) {
 
 	url := "http://localhost:8000/accounts/create"
 	method := "POST"
@@ -82,7 +83,7 @@ func createAccount(trace_no string, account_id string) {
 	}
 	data, err := protojson.Marshal(in)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Request: ", requestId, err)
 		return
 	}
 
@@ -90,7 +91,7 @@ func createAccount(trace_no string, account_id string) {
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Request: ", requestId, err)
 		return
 	}
 	req.Header.Add("Content-Type", "application/json")
@@ -98,15 +99,16 @@ func createAccount(trace_no string, account_id string) {
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Request: ", requestId, err)
+
 		return
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Request: ", requestId, err)
 		return
 	}
-	fmt.Println(string(body))
+	fmt.Println("Request: ", requestId, string(body))
 }
